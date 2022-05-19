457f6734414b4879088463f0fb805aeab712eed9
Yuuki <ichaloned@gmail.com>
2022-05-19 20:56:59 +0700

    Update sysinfo
    - Change sysinfo embed formatting.
    - Add fancy emojis.
    - Add storage information.

--- cogs/sysinfo.py
+++ cogs/sysinfo.py
@@ -23,15 +23,35 @@ class sysinfo(commands.Cog):
             interaction: discord.Interaction) -> None:
 
         CPU_usage = f"{psutil.cpu_percent(0.1)} %"
-        RAM_usage = f"{psutil.virtual_memory().available >> 20} MB / {psutil.virtual_memory().total >> 20} MB ({psutil.virtual_memory().percent} %)"
-        CPU_temp = f"Not Available on Windows"
+        RAM_usage = f"{(psutil.virtual_memory().total >> 20)-(psutil.virtual_memory().available >> 20)} MB ({100-psutil.virtual_memory().percent} %)"
+        CPU_temp = f"N/A"
         if(platform.system() != "Windows"):
             CPU_temp = f"{psutil.sensors_temperatures()['scpi_sensors'][0][1]} C°"
+        Disk_usage = f"{psutil.disk_usage('/').free >> 20} MB ({psutil.disk_usage('/').percent} %)"
 
         embed = discord.Embed(
-            title="System Information",
-            description=f"**CPU Usage**\n{CPU_usage}\n\n**RAM Usage**\n{RAM_usage}\n\n**Temperature (C°)**\n{CPU_temp}",
+            title="**<:workstation:976814353212903435> System Information**",
             color=discord.Colour.brand_red()
+        )
+        embed.add_field(
+            name=f"**<:processor:976813558438457395> CPU :                        **",
+            value=f"{CPU_usage}",
+            inline=False
+        )
+        embed.add_field(
+            name=f"**<:memory:976813933195329566> RAM :**",
+            value=f"{RAM_usage}",
+            inline=False
+        )
+        embed.add_field(
+            name=f"**:thermometer: Temp :                        **",
+            value=f"{CPU_temp}",
+            inline=False
+        )
+        embed.add_field(
+            name=f"**<:hdd:976818332940525610> Storage :**",
+            value=f"{Disk_usage}",
+            inline=False
         )
         await interaction.response.send_message(embed=embed)
 
